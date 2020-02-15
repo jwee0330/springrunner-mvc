618d6af805d226ff6a99c23a87f5329bf846c711
Jayden <jayden.lee@uneedcomms.com>
2020-02-15 18:32:27 +0900

    feature: day01 수업 종료

--- src/main/java/todoapp/web/WebMvcConfiguration.java
+++ src/main/java/todoapp/web/WebMvcConfiguration.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -29,7 +30,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
      * 스프링부트가 생성한 ContentNegotiatingViewResolver를 조작할 목적으로 작성된 컴포넌트
      */
     @Configuration
-    public static class ContentNegotiationCustomizer {
+    public static class ContentNegotiationCustomizer { 
 
     	@Autowired
         public void configurer(ContentNegotiatingViewResolver contentNegotiatingViewResolver) {
