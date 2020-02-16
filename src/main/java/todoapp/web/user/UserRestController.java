cf278b86098348c41183043079fe23484dd902ec
jwee0330 <jwee0330@gmail.com>
2020-02-16 16:44:58 +0900

    feature: UserRestController정리
    이제 @RolesAllowed 를 통해 세션 널 상태가 보호된다.

--- src/main/java/todoapp/web/user/UserRestController.java
+++ src/main/java/todoapp/web/user/UserRestController.java
@@ -1,11 +1,7 @@
 package todoapp.web.user;
 
-import java.util.Objects;
-
 import javax.annotation.security.RolesAllowed;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +13,8 @@ public class UserRestController {
 
 	@RolesAllowed(UserSession.ROLE_USER)
 	@GetMapping("/api/user/profile")
-	public ResponseEntity<UserProfile> profile(UserSession session) {
-		if (Objects.nonNull(session)) {
-			return ResponseEntity.ok(new UserProfile(session.getUser()));
-		}
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); 
+	public UserProfile profile(UserSession session) {
+		return new UserProfile(session.getUser());
 	}
 	
 }
