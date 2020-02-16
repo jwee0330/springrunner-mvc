1fd491cfedacc218c4de4952880dd94893ac9eb6
jwee0330 <jwee0330@gmail.com>
2020-02-16 14:29:23 +0900

    featrue: 패스워드 불일치, bindingResult error

--- src/main/java/todoapp/web/user/LoginController.java
+++ src/main/java/todoapp/web/user/LoginController.java
@@ -1,14 +1,21 @@
 package todoapp.web.user;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import todoapp.core.user.application.UserJoinder;
 import todoapp.core.user.application.UserPasswordVerifier;
 import todoapp.core.user.domain.UserEntityNotFoundException;
+import todoapp.core.user.domain.UserPasswordNotMatchedException;
 
 @Controller
 public class LoginController {
@@ -28,9 +35,15 @@ public class LoginController {
 	}
 	
 	@PostMapping("/login")
-	public String loginProcess(LoginCommand loginCommand) {
+	public String loginProcess(@Valid LoginCommand loginCommand, BindingResult bindingResult, Model model) {
 		
 		log.info("username: {}, password: {}", loginCommand.getUsername(), loginCommand.getPassword());
+		
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("bindingResult", bindingResult);
+			model.addAttribute("message", "사용자 입력 값이 올바르지 않습니다.");
+			return "login";
+		}
 		
 		try {
 			verifier.verify(loginCommand.getUsername(), loginCommand.getPassword());
@@ -41,8 +54,15 @@ public class LoginController {
 		return "redirect:/todos";
 	}
 	
+	@ExceptionHandler(UserPasswordNotMatchedException.class)
+	public String handleUserPasswordNotMatchedException(UserPasswordNotMatchedException error, Model model) {
+		model.addAttribute("message", "사용자 정보가 올바르지 않습니다.");
+		return "login";
+	}
+	
 	static class LoginCommand {
 		
+		@Size(min = 4, max = 20)
 		private String username;
 		private String password;
 		
